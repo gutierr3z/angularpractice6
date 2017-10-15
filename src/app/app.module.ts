92f1c4dcea4eabd673b9b727ad431ea138f4e71e
Juan Gutierrez <gutierr3z@gmail.com>
2017-10-15 17:52:37 -0400

    practice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { DataService } from './services/data.service';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './components/main/main.component';
